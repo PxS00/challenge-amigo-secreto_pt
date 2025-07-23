6eef3d123d421df562286ad84002b8ca5fcad519
PxS00 <lucas.rossonidd@gmail.com>
2025-07-23 15:25:28 -0300

    feat: adicionar amigos à lista com exibição e validação

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = []
+
+function adicionarAmigo() {
+    let amigo = document.getElementById('amigo')
+    
+    if (amigo.value == '') {
+        alert('Por favor, digite o nome de um amigo');
+        return;
+    }
+
+    amigos.push(amigo.value)
+
+    let lista = document.getElementById('listaAmigos')
+    
+    if (lista.textContent == '') {
+        lista.textContent = amigo.value;
+    } else {
+        lista.textContent = lista.textContent + ', ' + amigo.value;
+    }
+    amigo.value = '';
+}
